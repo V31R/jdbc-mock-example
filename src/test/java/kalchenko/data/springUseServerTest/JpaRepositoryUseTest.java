cb6e84a61746995452b2e928e4cccf6709bbcbf3
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2022-07-12 01:07:20 +0300

    Trying to make findAll work

--- src/test/java/kalchenko/data/springUseServerTest/JpaRepositoryUseTest.java
+++ src/test/java/kalchenko/data/springUseServerTest/JpaRepositoryUseTest.java
@@ -1,6 +1,9 @@
 package kalchenko.data.springUseServerTest;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import kalchenko.data.DataRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,15 +11,44 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@WireMockTest
 public class JpaRepositoryUseTest {
 
     @Autowired
     DataRepository dataRepository;
 
+    private static String host="http://localhost:";
+    private static String url="/sql-mock";
+    private static String uri;
+
+
+    @BeforeAll
+    static void startWireMock(WireMockRuntimeInfo wmRuntimeInfo){
+
+        uri = host + wmRuntimeInfo.getHttpPort() + url;
+
+    }
+
+    @Test
+    public void testWireMockSetUp(WireMockRuntimeInfo wmRuntimeInfo){
+
+        assertEquals(host + wmRuntimeInfo.getHttpPort(),wmRuntimeInfo.getHttpBaseUrl());
+
+    }
+
     @Test
     public void dataRepositorySmoke(){
 
         assertNotNull(dataRepository);
+
+    }
+
+    @Test
+    public void findAll(){
+
+        var result = dataRepository.findAll();
+
+        assertNotNull(result);
 
     }
 
